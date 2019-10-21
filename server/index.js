50b3e35895b9cfeb90a889ca51979be3ff1b4687
ValentinaJakobson <jakobsonvv@gmail.com>
2019-10-22 00:49:22 +0300

    cleaned

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
         cb(null, 'public')
     },
     filename: function (req, file, cb){
-        cb(null, Date.now() + '-' +file.originalname)
+        cb(null, file.originalname)
     }
 })
 
--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -32,23 +32,20 @@ class Uploader extends Component {
   
         
     render() {
-      
         return (
            <div>
-            {/* <form action="/upload" method="post" encType="multipart/form-data"> */}
-                        <div className="file-field input-field">
-                            <div className="btn">
-                                <span>File</span>
-                                <input type="file" required name="file_upload" onChange={this.onChangeHandler}/>
-                            </div>
-                            <div className="file-path-wrapper">
-                                <input className="file-path validate" type="text" />
-                            </div>
+                    <div className="file-field input-field">
+                        <div className="btn">
+                            <span>File</span>
+                            <input type="file" required name="file_upload" onChange={this.onChangeHandler}/>
                         </div>
+                    <div className="file-path-wrapper">
+                        <input className="file-path validate" type="text" />
+                    </div>
+                    </div>
                         
-                        <button type="submit" onClick={this.onClickHandler}>Upload</button>
+                    <button type="submit" onClick={this.onClickHandler}>Upload</button>
 
-                    {/* </form> */}
            </div>
         );
       }
